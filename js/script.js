e15008ea8970e61b67a7d6496793da93f836c549
dev0715 <webdev0715@gmail.com>
2024-07-23 21:57:31 +0900

    complete draft

--- js/script.js
+++ js/script.js
@@ -2,8 +2,14 @@
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
+    // document.querySelector(this.getAttribute("href")).scrollIntoView({
+    //   behavior: "smooth",
+    // });
+    document.getElementById("signup").style.display = "flex";
   });
+});
+
+document.getElementById("close-button").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("signup").style.display = "none";
 });
