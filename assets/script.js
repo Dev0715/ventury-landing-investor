d1d420f57c4d84f47a78ae6e4de501afc9e98100
dev0715 <webdev0715@gmail.com>
2024-07-26 00:46:00 +0900

    refactor signup page

--- assets/script.js
+++ assets/script.js
@@ -13,3 +13,17 @@ document.getElementById("close-button").addEventListener("click", function (e) {
   e.preventDefault();
   document.getElementById("signup").style.display = "none";
 });
+
+window.onload = function () {
+  var screenWidth = screen.width;
+  var screenHeight = screen.height;
+
+  var container = document.querySelector(".signup-container");
+  container.scrollTop = (container.scrollHeight - container.offsetHeight) / 2;
+
+  if (screenWidth > 1367) {
+    if (screenHeight > 600) {
+      container.style.alignItems = "center";
+    }
+  }
+};
