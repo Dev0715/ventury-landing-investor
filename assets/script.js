ffe163445adf2d6f9599f3c4953a863a38f71ff4
dev0715 <webdev0715@gmail.com>
2024-08-02 18:29:41 +0900

    update: re position signup-form

--- assets/script.js
+++ assets/script.js
@@ -2,28 +2,28 @@
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
-    // document.querySelector(this.getAttribute("href")).scrollIntoView({
-    //   behavior: "smooth",
-    // });
-    document.getElementById("signup").style.display = "flex";
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+    // document.getElementById("signup").style.display = "flex";
   });
 });
 
-document.getElementById("close-button").addEventListener("click", function (e) {
-  e.preventDefault();
-  document.getElementById("signup").style.display = "none";
-});
+// document.getElementById("close-button").addEventListener("click", function (e) {
+//   e.preventDefault();
+//   document.getElementById("signup").style.display = "none";
+// });
 
-window.onload = function () {
-  var screenWidth = screen.width;
-  var screenHeight = screen.height;
+// window.onload = function () {
+//   var screenWidth = screen.width;
+//   var screenHeight = screen.height;
 
-  var container = document.querySelector(".signup-container");
-  container.scrollTop = (container.scrollHeight - container.offsetHeight) / 2;
+//   var container = document.querySelector(".signup-container");
+//   container.scrollTop = (container.scrollHeight - container.offsetHeight) / 2;
 
-  if (screenWidth > 1367) {
-    if (screenHeight > 600) {
-      container.style.alignItems = "center";
-    }
-  }
-};
+//   if (screenWidth > 1367) {
+//     if (screenHeight > 600) {
+//       container.style.alignItems = "center";
+//     }
+//   }
+// };
